c99ff7ac25081d2aeec6e9248dddcdc03fb291e0
Pelle Andersson <pellejamas@gmail.com>
2021-04-29 20:43:50 +0200

    SP1- Skriva rad 2

--- Excel/src/main/java/excel/Excel.java
+++ Excel/src/main/java/excel/Excel.java
@@ -10,45 +10,55 @@ public class Excel {
 		//Create workbook in .xls format
 		Workbook workbook = new HSSFWorkbook();
 		//For .xslx workbooks use XSSFWoorkbok();
-		//Create Sheet
+		//Create Sheets
 		Sheet sh = workbook.createSheet("Decathlon");
 		Sheet sh2 = workbook.createSheet("Heptathlon");
-		//Create Row and Cell for Decathlon
-		Cell cell = sh.createRow(0).createCell(0);
-		Cell cell1 = sh.createRow(0).createCell(1);
-		Cell cell2 = sh.createRow(0).createCell(2);
-		Cell cell3 = sh.createRow(0).createCell(3);
-		Cell cell4 = sh.createRow(0).createCell(4);
-		Cell cell5 = sh.createRow(0).createCell(5);
-		Cell cell6 = sh.createRow(0).createCell(6);
-		Cell cell7 = sh.createRow(0).createCell(7);
-		Cell cell8 = sh.createRow(0).createCell(8);
-		Cell cell9 = sh.createRow(0).createCell(9);
-		Cell cell10 = sh.createRow(0).createCell(10);
-		Cell cell11 = sh.createRow(0).createCell(11);
-		Cell cell12 = sh.createRow(0).createCell(12);
-		Cell cell13 = sh.createRow(0).createCell(13);
-		Cell cell14 = sh.createRow(0).createCell(14);
+		//Create Rows and Cells for Decathlon ROW 1
+		Cell cellA1 = sh.createRow(0).createCell(0);
+		Cell cellB1 = sh.createRow(0).createCell(1);
+		Cell cellC1 = sh.createRow(0).createCell(2);
+		Cell cellD1 = sh.createRow(0).createCell(3);
+		Cell cellE1 = sh.createRow(0).createCell(4);
+		Cell cellF1 = sh.createRow(0).createCell(5);
+		Cell cellG1 = sh.createRow(0).createCell(6);
+		Cell cellH1 = sh.createRow(0).createCell(7);
+		Cell cellI1 = sh.createRow(0).createCell(8);
+		Cell cellJ1 = sh.createRow(0).createCell(9);
+		Cell cellK1 = sh.createRow(0).createCell(10);
+		Cell cellL1 = sh.createRow(0).createCell(11);
+		Cell cellM1 = sh.createRow(0).createCell(12);
+		Cell cellN1 = sh.createRow(0).createCell(13);
+		Cell cellO1 = sh.createRow(0).createCell(14);
 		
-		//Add value in a cell for Decathlon
-		cell.setCellValue("Number");
-		cell1.setCellValue("Name");
-		cell2.setCellValue("Event 1");
-		cell3.setCellValue("Event 2");
-		cell4.setCellValue("Event 3");
-		cell5.setCellValue("Event 4");
-		cell6.setCellValue("Event 5");
-		cell7.setCellValue("Day 1 points");
-		cell8.setCellValue("Event 6");
-		cell9.setCellValue("Event 7");
-		cell10.setCellValue("Event 8");
-		cell11.setCellValue("Event 9");
-		cell12.setCellValue("Event 10");
-		cell13.setCellValue("Day 2 points");
-		cell14.setCellValue("Total points");
+		//Add value in a cell for Decathlon ROW 1
+		cellA1.setCellValue("Number");
+		cellB1.setCellValue("Name");
+		cellC1.setCellValue("Event 1");
+		cellD1.setCellValue("Event 2");
+		cellE1.setCellValue("Event 3");
+		cellF1.setCellValue("Event 4");
+		cellG1.setCellValue("Event 5");
+		cellH1.setCellValue("Day 1 points");
+		cellI1.setCellValue("Event 6");
+		cellJ1.setCellValue("Event 7");
+		cellK1.setCellValue("Event 8");
+		cellL1.setCellValue("Event 9");
+		cellM1.setCellValue("Event 10");
+		cellN1.setCellValue("Day 2 points");
+		cellO1.setCellValue("Total points");
 		
-		//Write the same value of the cell into the console in java
-		System.out.println(cell.getRichStringCellValue().toString());
+		//Create Rows and Cells for Decathlon ROW 2
+		Cell cellA2 = sh.createRow(1).createCell(0);
+		Cell cellB2 = sh.createRow(1).createCell(1);
+		Cell cellH2 = sh.createRow(1).createCell(7);
+		Cell cellN2 = sh.createRow(1).createCell(13);
+		Cell cellO2 = sh.createRow(1).createCell(14);
+		//Add value in a cell for Decathlon ROW 2
+		cellA2.setCellValue("1:A");
+		cellB2.setCellValue("Pär Andersson");
+		cellH2.setCellValue("3000");
+		cellN2.setCellValue("4000");
+		cellO2.setCellFormula("SUM(H2+N2)");
 		
 		try {
 			//Write the output to file
